492f19108d0a73d0d88a756b6b737ef4894c8b9d
souravsingh122 <62555548+souravsingh122@users.noreply.github.com>
2025-08-03 00:59:35 +0530

    Update sendNotification.groovy

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -1,44 +1,19 @@
-def call(env) {
-    def commitId = ''
-    def commitMsg = ''
-    def diffFile = 'git_diff.txt'
-    def logFile = 'build_log.txt'
-    def recipients = 'souravsingh8917@gmail.com sourav.singh@shipease.in'
+@Library('jenkins-shared-lib') _
 
-    try {
-        commitId = sh(script: "git rev-parse HEAD", returnStdout: true).trim()
-        commitMsg = sh(script: "git log -1 --pretty=format:'%s'", returnStdout: true).trim()
-        sh(script: "git show ${commitId} > ${diffFile}")
-    } catch (Exception e) {
-        echo "Warning: Git commit info could not be retrieved. ${e}"
-        commitId = 'N/A'
-        commitMsg = 'N/A'
-        writeFile file: diffFile, text: 'Git diff not available due to error.'
+pipeline {
+    agent any
+
+    stages {
+        stage('Dummy Task') {
+            steps {
+                echo "Simulating build..."
+            }
+        }
     }
 
-    // Save Jenkins build logs
-    writeFile file: logFile, text: currentBuild.rawBuild.getLog().join('\n')
-
-    emailext(
-        to: recipients,
-        subject: "Jenkins Job - ${env.JOB_NAME} Build #${env.BUILD_NUMBER} Status: ${currentBuild.currentResult}",
-        body: """
-<html>
-<body>
-    <p><b>Job Name:</b> ${env.JOB_NAME}</p>
-    <p><b>Build Number:</b> ${env.BUILD_NUMBER}</p>
-    <p><b>Status:</b> ${currentBuild.currentResult}</p>
-    <p><b>Git Commit ID:</b> ${commitId}</p>
-    <p><b>Git Commit Message:</b> ${commitMsg}</p>
-    <p>See attached files for:</p>
-    <ul>
-        <li>💾 Full build logs</li>
-        <li>🔍 Git diff of latest commit</li>
-    </ul>
-</body>
-</html>
-""",
-        mimeType: 'text/html',
-        attachmentsPattern: "${logFile}, ${diffFile}"
-    )
+    post {
+        always {
+            sendNotification(env)
+        }
+    }
 }
